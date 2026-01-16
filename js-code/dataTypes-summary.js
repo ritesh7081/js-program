521d03e6a3ae02879fb1dcd1c863da41963edfd4
Ritesh Kumar <riteshjnv9838@gmail.com>
2026-01-16 16:11:03 +0530

    Some small changes in data types summary

--- js-code/dataTypes-summary.js
+++ js-code/dataTypes-summary.js
@@ -33,3 +33,28 @@ const myFunc = function () {
 
 console.log(typeof bigNumber); // bigint
 console.log(typeof myFunc); // function
+
+
+
+
+// **************************** Memory Allocation ****************************
+
+// Primitive Data Types are stored in Stack Memory
+// Refrence Data Types (Non -Primitive: Functions, Objects, Arrays) are stored in Heap Memory
+
+let myName = "Ritesh"; // stored in Stack Memory
+let anotherName = myName; // stored in Stack Memory 
+anotherName = "Kumar";
+
+console.log(myName); // Ritesh
+console.log(anotherName); // Kumar
+
+let userOne = {
+  email: "userone@example.com",
+  mobileNumber: "1234567890",
+}; // stored in Heap Memory
+
+let userTwo = userOne; // stored in Stack Memory (reference to the object in Heap Memory)
+userTwo.email = "usertwo@example.com";
+console.log(userOne.email); // usertwo@example.com
+console.log(userTwo.email); // usertwo@example.com
