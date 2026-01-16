e2ddcca7adf8303a63d4bbffedb67c6e30e5a734
Ritesh Kumar <riteshjnv9838@gmail.com>
2026-01-16 15:08:16 +0530

    conversionOperation

---
+++ js-code/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof (valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ritesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let floatNumber = 33.3434;
+console.log(typeof(floatNumber));
