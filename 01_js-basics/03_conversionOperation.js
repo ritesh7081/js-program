43e5290e8531c3e58a37a3e51aa71fa8af4b20b0
Ritesh Kumar <riteshjnv9838@gmail.com>
2026-01-17 17:52:17 +0530

    Small Changes

--- 01_js-basics/03_conversionOperation.js
+++ 01_js-basics/03_conversionOperation.js
@@ -48,10 +48,10 @@ let str2 = " Ritesh";
 let str3 = str1 + str2;
 // console.log(str3);
 
-// console.log("1" + 2); // "12"
-// console.log(1 + "2"); // "12"
-// console.log("1" + 2 + 2); // "122"
-// console.log(1 + 2 + "2"); // "32"
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log("1" + 2 + 2); // "122"
+console.log(1 + 2 + "2"); // "32"
 
 console.log(+true); // 1
 console.log(+false); // 0
